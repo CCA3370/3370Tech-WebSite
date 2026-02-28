7a20d252d9f5750bb16948a2ba5cf9186cc22a43
CCA3370 <ryn905010@gmail.com>
2026-02-28 16:49:32 +0800

    feat: add GET route to redirect to XFast Manager download page

---
+++ src/app/org/xfastmanager/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.redirect(
+    'https://forums.x-plane.org/files/file/98845-linwinmacxfast-managerdrag-drop-addon-installer-auto-scenery-sorting-more/',
+  );
+}
